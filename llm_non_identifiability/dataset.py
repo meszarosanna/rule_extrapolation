ee5eb33d8bc0ff92a5831863ca87da7b4369a2a3
Patrik Reizinger <prmedia17@gmail.com>
2023-12-30 17:10:42 +0100

    dataset to long

--- llm_non_identifiability/dataset.py
+++ llm_non_identifiability/dataset.py
@@ -9,7 +9,7 @@ from llm_non_identifiability.data import pad
 class GrammarDataset(Dataset):
     def __init__(self, data):
         # pad the data
-        self.data = torch.from_numpy(pad(data))
+        self.data = torch.from_numpy(pad(data)).long()
         self.labels = self.data.clone()
 
     def __len__(self):
