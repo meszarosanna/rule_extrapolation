08da8272f3189cf2febd701c6e5d8efd4a82c741
Patrik Reizinger <prmedia17@gmail.com>
2023-12-30 19:49:37 +0100

    dataset size fiztures

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,3 +9,18 @@ def max_length():
 @pytest.fixture
 def n():
     return 20
+
+
+@pytest.fixture
+def n_train():
+    return 128
+
+
+@pytest.fixture
+def n_val():
+    return 64
+
+
+@pytest.fixture
+def n_test():
+    return 32
