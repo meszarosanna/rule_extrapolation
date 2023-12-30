9c162a5ce76e81a7213a5a3aea92a0c7b742f206
Patrik Reizinger <prmedia17@gmail.com>
2023-12-30 17:11:00 +0100

    dataset to long test

---
+++ tests/test_dataset.py
@@ -0,0 +1,8 @@
+from llm_non_identifiability.dataset import GrammarDataset
+import torch
+
+
+def test_data_is_long():
+    data = GrammarDataset([[1, 2, 3], [4, 5], [6, 7, 8, 9]])
+    assert data.data.dtype == torch.long
+    assert data.labels.dtype == torch.long
