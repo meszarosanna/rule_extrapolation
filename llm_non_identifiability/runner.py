c8dfebc2c28fc97d28b3b46a848f6b54153c1880
Patrik Reizinger <prmedia17@gmail.com>
2023-12-30 19:50:14 +0100

    pl runner

--- llm_non_identifiability/runner.py
+++ llm_non_identifiability/runner.py
@@ -2,154 +2,8 @@
 import torch
 import torch.nn as nn
 
-import math
-
-from llm_non_identifiability.data import generate_aNbN_grammar_data, batchify_data, PAD
-
-
-class PositionalEncoding(nn.Module):
-    def __init__(self, dim_model, dropout_p, max_len):
-        super().__init__()
-        # Modified version from: https://pytorch.org/tutorials/beginner/transformer_tutorial.html
-        # max_len determines how far the position can have an effect on a token (window)
-
-        # Info
-        self.dropout = nn.Dropout(dropout_p)
-
-        # Encoding - From formula
-        pos_encoding = torch.zeros(max_len, dim_model)
-        positions_list = torch.arange(0, max_len, dtype=torch.float).view(
-            -1, 1
-        )  # 0, 1, 2, 3, 4, 5
-        division_term = torch.exp(
-            torch.arange(0, dim_model, 2).float() * (-math.log(10000.0)) / dim_model
-        )  # 1000^(2i/dim_model)
-
-        # PE(pos, 2i) = sin(pos/1000^(2i/dim_model))
-        pos_encoding[:, 0::2] = torch.sin(positions_list * division_term)
-
-        # PE(pos, 2i + 1) = cos(pos/1000^(2i/dim_model))
-        pos_encoding[:, 1::2] = torch.cos(positions_list * division_term)
-
-        # Saving buffer (same as parameter without gradients needed)
-        pos_encoding = pos_encoding.unsqueeze(0).transpose(0, 1)
-        self.register_buffer("pos_encoding", pos_encoding)
-
-    def forward(self, token_embedding: torch.tensor) -> torch.tensor:
-        # Residual connection + pos encoding
-        return self.dropout(
-            token_embedding + self.pos_encoding[: token_embedding.size(0), :]
-        )
-
-
-class Transformer(nn.Module):
-    """
-    Model from "A detailed guide to Pytorch's nn.Transformer() module.", by
-    Daniel Melchor: https://medium.com/@danielmelchor/a-detailed-guide-to-pytorchs-nn-transformer-module-c80afbc9ffb1
-    """
-
-    # Constructor
-    def __init__(
-        self,
-        num_tokens,
-        dim_model,
-        num_heads,
-        num_encoder_layers,
-        num_decoder_layers,
-        dropout_p,
-    ):
-        super().__init__()
-
-        # INFO
-        self.model_type = "Transformer"
-        self.dim_model = dim_model
-
-        # LAYERS
-        self.positional_encoder = PositionalEncoding(
-            dim_model=dim_model, dropout_p=dropout_p, max_len=5000
-        )
-        self.embedding = nn.Embedding(num_tokens, dim_model)
-        self.transformer = nn.Transformer(
-            d_model=dim_model,
-            nhead=num_heads,
-            num_encoder_layers=num_encoder_layers,
-            num_decoder_layers=num_decoder_layers,
-            dropout=dropout_p,
-        )
-        self.out = nn.Linear(dim_model, num_tokens)
-
-    def forward(self, src, tgt, tgt_mask=None, src_pad_mask=None, tgt_pad_mask=None):
-        # Src size must be (batch_size, src sequence length)
-        # Tgt size must be (batch_size, tgt sequence length)
-
-        # assert src.shape[0] == BS
-        # assert tgt.shape[0] == BS
-
-        # Embedding + positional encoding - Out size = (batch_size, sequence length, dim_model)
-
-        src = self.embedding(src) * math.sqrt(self.dim_model)
-        tgt = self.embedding(tgt) * math.sqrt(self.dim_model)
-        src = self.positional_encoder(src)
-        tgt = self.positional_encoder(tgt)
-
-        # We could use the parameter batch_first=True, but our KDL version doesn't support it yet, so we permute
-        # to obtain size (sequence length, batch_size, dim_model),
-        src = src.permute(1, 0, 2)
-        tgt = tgt.permute(1, 0, 2)
-
-        # Transformer blocks - Out size = (sequence length, batch_size, num_tokens)
-        transformer_out = self.transformer(
-            src,
-            tgt,
-            tgt_mask=tgt_mask,
-            src_key_padding_mask=src_pad_mask,
-            tgt_key_padding_mask=tgt_pad_mask,
-        )
-        out = self.out(transformer_out)
-
-        return out
-
-    def get_tgt_mask(self, size) -> torch.tensor:
-        # Generates a squeare matrix where the each row allows one word more to be seen
-        mask = torch.tril(torch.ones(size, size) == 1)  # Lower triangular matrix
-        mask = mask.float()
-        mask = mask.masked_fill(mask == 0, float("-inf"))  # Convert zeros to -inf
-        mask = mask.masked_fill(mask == 1, float(0.0))  # Convert ones to 0
-
-        # EX for size=5:
-        # [[0., -inf, -inf, -inf, -inf],
-        #  [0.,   0., -inf, -inf, -inf],
-        #  [0.,   0.,   0., -inf, -inf],
-        #  [0.,   0.,   0.,   0., -inf],
-        #  [0.,   0.,   0.,   0.,   0.]]
-
-        return mask
-
-    def create_pad_mask(
-        self, matrix: torch.tensor, pad_token: int = PAD
-    ) -> torch.tensor:
-        # If matrix = [1,2,3,0,0,0] where pad_token=0, the result mask is
-        # [False, False, False, True, True, True]
-        return matrix == pad_token
-
-
-train_data = generate_aNbN_grammar_data(9000)
-val_data = generate_aNbN_grammar_data(3000)
-
-train_dataloader = batchify_data(train_data)
-val_dataloader = batchify_data(val_data)
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model = Transformer(
-    num_tokens=5,
-    dim_model=8,
-    num_heads=4,
-    num_encoder_layers=2,
-    num_decoder_layers=2,
-    dropout_p=0.1,
-).to(device)
-opt = torch.optim.SGD(model.parameters(), lr=0.01)
-loss_fn = nn.CrossEntropyLoss()
+from llm_non_identifiability.data import generate_aNbN_grammar_data, batchify_data
+from llm_non_identifiability.model import Transformer
 
 
 def train_loop(model, opt, loss_fn, dataloader):
@@ -260,11 +114,6 @@ def fit(model, opt, loss_fn, train_dataloader, val_dataloader, epochs):
     return train_loss_list, validation_loss_list
 
 
-train_loss_list, validation_loss_list = fit(
-    model, opt, loss_fn, train_dataloader, val_dataloader, 125
-)
-
-
 def predict(model, input_sequence, max_length=10, SOS_token=2, EOS_token=3):
     """
     Method from "A detailed guide to Pytorch's nn.Transformer() module.", by
@@ -306,16 +155,103 @@ def predict(model, input_sequence, max_length=10, SOS_token=2, EOS_token=3):
     return y_input.view(-1).tolist()
 
 
-# Here we test some examples to observe how the model predicts
-examples = [
-    torch.tensor([[2, 0, 0, 0, 0, 1, 1, 1, 1, 3]], dtype=torch.long, device=device),
-    torch.tensor([[2, 0, 0, 0, 1, 1, 1, 3]], dtype=torch.long, device=device),
-    torch.tensor([[2, 0, 1, 3]], dtype=torch.long, device=device),
-]
+import pytorch_lightning as pl
 
-for idx, example in enumerate(examples):
-    result = predict(model, example)
-    print(f"Example {idx}")
-    print(f"Input: {example.view(-1).tolist()[1:-1]}")
-    print(f"Continuation: {result[1:-1]}")
-    print()
+
+class LightningGrammarModule(pl.LightningModule):
+    def __init__(self, lr=0.01, device="cuda" if torch.cuda.is_available() else "cpu"):
+        super().__init__()
+        self.save_hyperparameters()
+
+        self.hparams.loss_fn = nn.CrossEntropyLoss()
+        self.model = Transformer(
+            num_tokens=5,
+            dim_model=8,
+            num_heads=4,
+            num_encoder_layers=2,
+            num_decoder_layers=2,
+            dropout_p=0.1,
+        )
+
+    def configure_optimizers(self):
+        return torch.optim.SGD(self.model.parameters(), lr=self.hparams.lr)
+
+    def training_step(self, batch, batch_idx):
+        panel_name = "Train"
+        _, _, _, loss = self._forward(batch)
+        self.log(f"{panel_name}/loss", loss)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        panel_name = "Val"
+        X, y, pred, loss = self._forward(batch)
+
+        self.log(f"{panel_name}/loss", loss)
+
+        return loss
+
+    def _forward(self, batch):
+        X, y = batch
+
+        # Now we shift the tgt by one so with the <SOS> we predict the token at pos 1
+        y_input = y[:, :-1]
+        y_expected = y[:, 1:]
+
+        # Get mask to mask out the next words
+        sequence_length = y_input.size(1)
+        tgt_mask = self.model.get_tgt_mask(sequence_length).to(self.hparams.device)
+
+        # Standard training except we pass in y_input and tgt_mask
+        pred = self.model(
+            X,
+            y_input,
+            tgt_mask,
+            self.model.create_pad_mask(X, 4),
+            self.model.create_pad_mask(y_input, 4),
+        )
+
+        # Permute pred to have batch size first again
+        pred = pred.permute(1, 2, 0)
+
+        loss = self.hparams.loss_fn(pred, y_expected)
+
+        return X, y, pred, loss
+
+
+if __name__ == "__main__":
+    train_data = generate_aNbN_grammar_data(9000)
+    val_data = generate_aNbN_grammar_data(3000)
+
+    train_dataloader = batchify_data(train_data)
+    val_dataloader = batchify_data(val_data)
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = Transformer(
+        num_tokens=5,
+        dim_model=8,
+        num_heads=4,
+        num_encoder_layers=2,
+        num_decoder_layers=2,
+        dropout_p=0.1,
+    ).to(device)
+    opt = torch.optim.SGD(model.parameters(), lr=0.01)
+    loss_fn = nn.CrossEntropyLoss()
+
+    train_loss_list, validation_loss_list = fit(
+        model, opt, loss_fn, train_dataloader, val_dataloader, 125
+    )
+
+    # Here we test some examples to observe how the model predicts
+    examples = [
+        torch.tensor([[2, 0, 0, 0, 0, 1, 1, 1, 1, 3]], dtype=torch.long, device=device),
+        torch.tensor([[2, 0, 0, 0, 1, 1, 1, 3]], dtype=torch.long, device=device),
+        torch.tensor([[2, 0, 1, 3]], dtype=torch.long, device=device),
+    ]
+
+    for idx, example in enumerate(examples):
+        result = predict(model, example)
+        print(f"Example {idx}")
+        print(f"Input: {example.view(-1).tolist()[1:-1]}")
+        print(f"Continuation: {result[1:-1]}")
+        print()
