d026c73911a9be6e2f22fa295cffbf1a5d3dd993
Patrik Reizinger <prmedia17@gmail.com>
2023-12-30 16:48:19 +0100

    padding function test

--- tests/test_data.py
+++ tests/test_data.py
@@ -2,6 +2,8 @@ from llm_non_identifiability.data import (
     generate_aNbN_grammar_data,
     generate_abN_grammar_data,
     generate_aN_bM_grammar_data,
+    pad,
+    PAD,
 )
 
 import numpy as np
@@ -41,3 +43,13 @@ def test_aNbM_grammar_as_before_bs(n, max_length):
         # find the last a
         last_a = np.where(sequence == 0)[0][-1]
         assert first_b > last_a
+
+
+def test_pad_varying_sequence_lengths():
+    data = [[1, 2, 3], [4, 5], [6, 7, 8, 9]]
+
+    expected_result = [[1, 2, 3, PAD], [4, 5, PAD, PAD], [6, 7, 8, 9]]
+    result = pad(data)
+
+    # check that the result is as expected with .all()
+    assert (np.array(result) == np.array(expected_result)).all()
