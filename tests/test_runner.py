d5fac6133308b63c232127eaa7673311a329127e
Patrik Reizinger <prmedia17@gmail.com>
2024-01-04 11:22:20 +0100

    test_predict_inner

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -3,6 +3,8 @@ from pytorch_lightning.trainer import Trainer
 from llm_non_identifiability.datamodule import GrammarDataModule
 from llm_non_identifiability.runner import LightningGrammarModule
 
+import torch
+
 
 def test_fit(n_train, n_val, n_test):
     trainer = Trainer(fast_dev_run=True)
@@ -18,3 +20,19 @@ def test_predict(n_train, n_val, n_test):
     trainer.fit(runner, datamodule=dm)
 
     trainer.predict(runner, datamodule=dm)
+
+
+def test_predict_inner(max_length):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    runner = LightningGrammarModule()
+
+    # Here we test some examples to observe how the model predicts
+    examples = [
+        torch.tensor([[2, 0, 0, 0, 0, 1, 1, 1, 1, 3]], dtype=torch.long, device=device),
+        torch.tensor([[2, 0, 0, 0, 1, 1, 1, 3]], dtype=torch.long, device=device),
+        torch.tensor([[2, 0, 1, 3]], dtype=torch.long, device=device),
+    ]
+
+    for idx, example in enumerate(examples):
+        runner._predict(max_length=max_length, src=example)
