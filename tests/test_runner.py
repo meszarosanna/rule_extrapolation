d6b137f56408dfce99c859a0cd8b3eceb9deea00
Patrik Reizinger <prmedia17@gmail.com>
2024-01-04 11:14:32 +0100

    test_predict

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -4,8 +4,17 @@ from llm_non_identifiability.datamodule import GrammarDataModule
 from llm_non_identifiability.runner import LightningGrammarModule
 
 
-def test_runner(n_train, n_val, n_test):
+def test_fit(n_train, n_val, n_test):
     trainer = Trainer(fast_dev_run=True)
     runner = LightningGrammarModule()
     dm = GrammarDataModule(n_train=n_train, n_val=n_val, n_test=n_test)
     trainer.fit(runner, datamodule=dm)
+
+
+def test_predict(n_train, n_val, n_test):
+    trainer = Trainer(fast_dev_run=True)
+    runner = LightningGrammarModule()
+    dm = GrammarDataModule(n_train=n_train, n_val=n_val, n_test=n_test)
+    trainer.fit(runner, datamodule=dm)
+
+    trainer.predict(runner, datamodule=dm)
