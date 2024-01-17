8c12ac0a1650f3364d1ad273150bdc48c765ed4e
Patrik Reizinger <prmedia17@gmail.com>
2024-01-17 19:46:13 +0100

    masking functions from methods

--- llm_non_identifiability/model.py
+++ llm_non_identifiability/model.py
@@ -41,6 +41,31 @@ class PositionalEncoding(nn.Module):
         )
 
 
+def get_tgt_mask(size) -> torch.Tensor:
+    # Generates a squeare matrix where the each row allows one word more to be seen
+    mask = torch.tril(torch.ones(size, size) == 1)  # Lower triangular matrix
+    mask = mask.float()
+    mask = mask.masked_fill(mask == 0, float("-inf"))  # Convert zeros to -inf
+    mask = mask.masked_fill(mask == 1, float(0.0))  # Convert ones to 0
+
+    # EX for size=5:
+    # [[0., -inf, -inf, -inf, -inf],
+    #  [0.,   0., -inf, -inf, -inf],
+    #  [0.,   0.,   0., -inf, -inf],
+    #  [0.,   0.,   0.,   0., -inf],
+    #  [0.,   0.,   0.,   0.,   0.]]
+
+    return mask
+
+
+def create_pad_mask(
+    matrix: torch.Tensor, pad_token: int = PAD_token.item()
+) -> torch.Tensor:
+    # If matrix = [1,2,3,0,0,0] where pad_token=0, the result mask is
+    # [False, False, False, True, True, True]
+    return matrix == pad_token
+
+
 class Transformer(nn.Module):
     """
     Model from "A detailed guide to Pytorch's nn.Transformer() module.", by
