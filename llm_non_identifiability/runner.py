2caa7a51860c95f2f39eeb64bd0e659ddda9c191
Patrik Reizinger <prmedia17@gmail.com>
2024-04-30 17:34:42 +0200

    arg linking fix

--- llm_non_identifiability/runner.py
+++ llm_non_identifiability/runner.py
@@ -54,7 +54,7 @@ class LightningGrammarModule(pl.LightningModule):
         offline: bool = False,
         next_token_pick_mode: str = "max",
         layer_norm_eps: float = 2e-4,
-        grammar: str = "aNbN",
+        grammar: str = "aNbNcN",
         max_data_length: int = 256,
         batch_size: int = 64,
         relu_rescale: float = 1.0,
